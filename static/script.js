53256870c71e44f614cc32607da4005da767c66f
j3iiifn <j3iiifn@gmail.com>
2020-07-27 15:18:28 +0900

    Improve tooltips.

--- static/script.js
+++ static/script.js
@@ -23,10 +23,15 @@ function drawChartLast24Hours(wattPerMinuteLast24Hours) {
                 borderColor: [
                     'rgba(255, 99, 132, 1)'
                 ],
-                borderWidth: 1
+                borderWidth: 1,
+                pointHitRadius: 10
             }]
         },
         options: {
+            tooltips: {
+                intersect: false,
+                mode: 'x'
+            },
             elements: {
                 line: {
                     tension: 0
@@ -73,7 +78,8 @@ function drawChartLast3Days(wattHourLast3Days, sumBillsPer12HoursLast3Days) {
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 1,
-                    steppedLine: 'before'
+                    steppedLine: 'before',
+                    pointHitRadius: 5
                 },
                 {
                     type: 'line',
@@ -82,11 +88,16 @@ function drawChartLast3Days(wattHourLast3Days, sumBillsPer12HoursLast3Days) {
                     data: wattHourLast3Days,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
+                    borderWidth: 1,
+                    pointHitRadius: 5
                 }
             ]
         },
         options: {
+            tooltips: {
+                intersect: false,
+                mode: 'x'
+            },
             elements: {
                 line: {
                     tension: 0
